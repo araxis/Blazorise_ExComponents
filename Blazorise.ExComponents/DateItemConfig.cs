9be7cdd695eb9c8dbd171779149eb07ee6a83b75
meisam alifallahi <meisamalifallahi@hotmail.com>
2022-03-28 13:20:23 +0430

    Add Calendar Component

---
+++ Blazorise.ExComponents/DateItemConfig.cs
@@ -0,0 +1,3 @@
+﻿namespace Blazorise.ExComponents;
+
+public record DateItemConfig(IFluentBorder Border, IFluentSpacing Padding, TextColor TextColor, string BackgroundColor);
---
+++ Blazorise.ExComponents/Extensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Blazorise.ExComponents;
+
+public static class Extensions
+{
+    public static bool IsToday(this DateTime date) => DateTime.Now.DayOfYear == date.DayOfYear;
+}
