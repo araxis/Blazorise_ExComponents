063d5a8315e06b4573bd8a6d3ba3eaf724454f10
meisam alifallahi <meisamalifallahi@hotmail.com>
2022-04-05 22:31:23 +0430

    add ExImage component

---
+++ Blazorise.ExComponents/Utilities/MeasureValue.cs
@@ -0,0 +1,22 @@
+﻿namespace Blazorise.ExComponents.Utilities;
+
+public record MeasureValue
+{
+    internal MeasureValue(double value, string unit)
+    {
+        this.Value = value;
+        this.Unit = unit;
+    }
+
+    public override string ToString() => $"{Value}{Unit}";
+    public double Value { get; init; }
+    public string Unit { get; init; }
+
+    public void Deconstruct(out double value, out string unit)
+    {
+        value = this.Value;
+        unit = this.Unit;
+    }
+
+    public static MeasureValue operator /(MeasureValue param, int value) => param with { Value =param.Value/value };
+}
---
+++ Blazorise.ExComponents/wwwroot/js/utilities.js
@@ -0,0 +1,13 @@
+﻿export function GetElementSize(element) {
+   //const elementSizInfo = getBoundingClientRect();
+   //     return {
+   //         width: elementSizInfo.width,
+   //         height: elementSizInfo.height
+   //     };
+
+   return {
+                width: element.offsetWidth,
+                height: element.offsetHeight
+            };
+
+};
